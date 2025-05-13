dc79ff07b9b885a06fa92f6f3cc023d1f53ed302
chaulinmatadordeproco <chaulinmatadordeproco@gmail.com>
2025-05-13 11:08:00 -0300

    fizemos algumas alterações e bum conseguimos

--- controllers/filas.controller.js
+++ controllers/filas.controller.js
@@ -1,12 +1,25 @@
 const mysql = require('../mysql');
 
-exports.entrarFila = async (req, res, next) => {
+exports.verificarBrinquedos = async (req, res, next) =>{
     try{
         const resultados = await mysql.execute(`
-            INSERT INTO lines (id_user, id_rides) VALUES(?,?)
-            `, [res.locals.idUsuario, req.params.idRide]);
-            return res.status(201).send({"Mensagens":resultados})
+            SELECT * FROM rides WHERE id = ?`, [req.params.idRide]);
+
+        if (resultados.length == 0){
+            return res.status(404).send({"Mensagem": "Brinquedo não encontrado"})
+        }
     }catch (error){
-        return res.status(500).send(error)
+        return res.status(500).send({"Mensagem": error})
+    }
+}
+
+exports.entrarFila = async (req, res) => {
+    try{
+        const resultados = await mysql.execute(`
+            INSERT INTO hopi_hari_db.lines (id_user, id_rides) VALUES(?,?)
+            `, [res.locals.idUsuario, Number(req.params.idRide)]);
+            return res.status(201).send({"Mensagens":resultados});
+    }catch (error){
+        return res.status(500).send(error);
     }
 }
--- routes/filas.route.js
+++ routes/filas.route.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const login = require("../middleware/usuarios.middleware")
+const filasController = require("../controllers/filas.controller");
 
-router.post("/", login.required, () => {console.log("Rota de fila")});
+router.post("/:idRide",
+    login.required,
+    filasController.verificarBrinquedos,
+    filasController.entrarFila
+)
 
 module.exports = router;
