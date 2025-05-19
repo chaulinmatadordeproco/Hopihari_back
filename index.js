216351e7460234ae3321fa197b05990d392a65a0
ChaulinMatadordePorco <chaulinmatadordeproco@gmail.com>
2025-05-19 16:22:01 -0300

    pegue essa alteração aq no git

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 
 const usuariosRoute = require("./routes/usuarios.route");
 const filasRoute = require("./routes/filas.route");
+const notificationsRoute = require("./routes/notification.route")
 
 app.use(cors());
 app.use(helmet());
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/usuarios", usuariosRoute);
-app.use("/filas", filasRoute)
+app.use("/filas", filasRoute);
+app.use("/Notifications", notificationsRoute);
 
 module.exports = app;
---
+++ routes/notification.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const login = require("../middleware/usuarios.middleware")
+const notificationsController = require("../controllers/notification.controller");
+
+router.get("/", login.required, notificationsController.getNotificacoes);
+
+module.exports = router;
